3ec90d70ab104353e959b2091e98131d24c6bb86
gunavardhan27 <bgunavardhan27@gmail.com>
2024-07-21 22:00:34 +0530

    creating subscirber

--- backend/notify.py
+++ backend/notify.py
@@ -11,12 +11,13 @@ api_key = "cc88a54a19c6aef6a95fef6be5b829fd"
     recipients="<SUBSCRIBER_IDENTIFIER>", # The subscriber ID can be gotten from the dashboard.
     payload={},  # Your custom Novu payload goes here
 )'''
-
+subscriber_api = SubscriberApi(url,api_key)
 def create_subscriber():
     subscriber = SubscriberDto(
     subscriber_id = '23',
     email = 'bgunavardhan27@gmail.com'
     )
-    sub = SubscriberApi(url,api_key).create(subscriber)
+    sub = subscriber_api.create(subscriber)
     print(sub)
-    print(SubscriberApi(url,api_key).list())
+    #print(SubscriberApi(url,api_key).list())
+
